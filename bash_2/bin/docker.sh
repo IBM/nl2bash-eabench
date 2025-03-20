65fb3c652757a763425ec4b4c9cfb251bd033408
Brent Paulovicks <ovicks@us.ibm.com>
2025-03-20 15:57:23 -0400

    Patched docker.sh to catch timeouts in bash_2 & bash_3

--- bash_2/bin/docker.sh
+++ bash_2/bin/docker.sh
@@ -14,10 +14,29 @@ userargs="--workdir=/home/test"
 mntargs="--volume $(pwd)/${work_dir}/home:/home:Z"
 #logargs="--log-driver=k8s-file --log-opt=path=$(pwd)/${work_dir}/podman.log"
 
+if [ -f ${test_set}/${test}/podman.opts ]
+then
+    tmo=$(sed 's+.*timeout-\([0-9][0-9]*\).*+\1+p' ${test_set}/${test}/podman.opts)
+    if [ -z "$tmo" ]
+    then
+        tmo=10
+    fi
+else
+    tmo=10
+fi
+
 rm -f ${work_dir}/podman.log
 
-docker run --rm ${userargs} ${userns} ${mntargs} ${logargs} ${podopts} ${container} ./test.sh > >(sed -e "s+^+ stdout F +" > ${work_dir}/docker_stdout.log) 2> >(sed -e "s+^+ stderr F +" > ${work_dir}/docker_stderr.log)
+docker run --rm ${userargs} ${userns} ${mntargs} ${logargs} ${podopts} ${container} timeout ${tmo} ./test.sh > >(sed -e "s+^+ stdout F +" > ${work_dir}/docker_stdout.log) 2> >(sed -e "s+^+ stderr F +" > ${work_dir}/docker_stderr.log)
 rc=$?
+
+#
+# Catch timeout rc and map to 255, same as podman
+#
+if [ $rc == 124 ]
+then
+    rc=255
+fi
 
 cat ${work_dir}/docker_stdout.log ${work_dir}/docker_stderr.log > ${work_dir}/podman.log
 rm  ${work_dir}/docker_stdout.log ${work_dir}/docker_stderr.log
